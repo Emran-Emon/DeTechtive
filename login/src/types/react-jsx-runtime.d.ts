2dba211db8addcc0f12376a3b7695e0c3a8c259a
nudarnawal <nudar.nawal@gmail.com>
2025-11-04 00:44:13 +0600

    Delete login directory

--- login/src/types/react-jsx-runtime.d.ts
+++
@@ -1,9 +0,0 @@
-// Minimal JSX runtime shim so TS can resolve 'react/jsx-runtime'
-
-declare module 'react/jsx-runtime' {
-  export const jsx: any;
-  export const jsxs: any;
-  export const Fragment: any;
-}
-
-
